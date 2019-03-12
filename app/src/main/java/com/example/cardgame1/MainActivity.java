1661b3f03e094b69c1d5fe1139cfd11c09e4cbc2
student <1440401918@qq.com>
2019-03-12 08:27:17 +0800

    first commit

---
+++ app/src/main/java/com/example/cardgame1/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.cardgame1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int flipCount = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button card = (Button)v;
+                if (card.getText() =="") {
+                    card.setText("♥️♥️♥️");
+                    card.setBackgroundResource(R.drawable.blankcard);
+                }else{
+                    card.setText("");
+                    card.setBackgroundResource(R.drawable.stanfordtree);
+                }
+                flipCount++;
+                TextView textView = findViewById(R.id.textView);
+                textView.setText("已经翻牌"+flipCount+"次");
+            }
+        });
+    }
+}
