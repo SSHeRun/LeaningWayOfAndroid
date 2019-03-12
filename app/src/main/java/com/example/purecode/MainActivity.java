d62ffec778301bd931f6f3c3d2de67267df88db4
student <1440401918@qq.com>
2019-03-12 10:19:22 +0800

    add debug

--- app/src/main/java/com/example/purecode/MainActivity.java
+++ app/src/main/java/com/example/purecode/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.purecode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +19,21 @@ public class MainActivity extends AppCompatActivity {
         linearLayout.setOrientation(LinearLayout.VERTICAL);
         linearLayout.setGravity(Gravity.CENTER);
 
-        TextView textView = new TextView(this);
+        final TextView textView = new TextView(this);
         textView.setText("hello world");
         linearLayout.addView(textView);
 
         Button button = new Button( this);
         button.setText("click me");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("I am Click");
+                Toast.makeText(MainActivity.this,"I am Click!" ,Toast.LENGTH_LONG).show();
+                //一种调试的方法，可以在直接手机上显示
+            }
+        });
+
         linearLayout.addView(button);
 
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
