5781f7e20b8ad5ba10b97abec99ed51df913a177
Thrinity <1440401918@qq.com>
2019-03-12 20:08:11 +0800

    Do test on my phone

--- MainActivity.java
+++ MainActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     //主活动
     int flipCount = 0;
     //定义构造方法
@@ -16,26 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //绑定Button资源
         btn = (Button) findViewById(R.id.button);
-        myListenerThird();
+        //设置Button监听
+        btn.setOnClickListener(this);
     }
 
-    private void myListenerThird() {
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Button card = (Button) v;
-                if (card.getText() == "") {
-                    card.setText("A🖤");
-                    card.setBackgroundResource(R.drawable.blankcard);
-                } else {
-                    card.setText("");
-                    card.setBackgroundResource(R.drawable.stanfordtree);
-                }
-                flipCount++;
-                TextView textView = (TextView) findViewById(R.id.textView);
-                textView.setText("已经翻牌了" + flipCount + "次");
-            }
-        });
+
+    @Override
+    public void onClick(View v) {
+        Button card = (Button) v;
+        if (card.getText() == "") {
+            card.setText("A🖤");
+            card.setBackgroundResource(R.drawable.blankcard);
+        } else {
+            card.setText("");
+            card.setBackgroundResource(R.drawable.stanfordtree);
+        }
+        flipCount++;
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setText("已经翻牌了" + flipCount + "次");
     }
 }
