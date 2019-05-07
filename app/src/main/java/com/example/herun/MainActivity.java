f1362f45746a3eed9a7ae6e3d1ec194c418631a5
WYF <1714436520@qq.com>
2019-05-07 09:22:00 +0800

    完成加的功能

--- app/src/main/java/com/example/herun/MainActivity.java
+++ app/src/main/java/com/example/herun/MainActivity.java
@@ -2,17 +2,33 @@ package com.example.herun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText number1 ;
     EditText number2 ;
-    EditText result ;
+    TextView result ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        number1 = findViewById(R.id.editText1);
+        number2 = findViewById(R.id.editText2);
+        result = findViewById(R.id.textView_result);
+    }
+
+    public void add(View view){
+        int num1 = Integer.parseInt(number1.getText().toString());
+        int num2 = Integer.parseInt(number2.getText().toString());
+        int all = num1+num2;
+        result.setText("Result is:"+all);
+    }
+    public void history(View view){
+
     }
 }
