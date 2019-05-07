d7aac20fb0e89f3b7f9489d24cb897257460d4f3
WYF <1714436520@qq.com>
2019-05-07 09:12:41 +0800

    ui

--- app/src/main/java/com/example/herun/MainActivity.java
+++ app/src/main/java/com/example/herun/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.herun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText number1 ;
+    EditText number2 ;
+    EditText result ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
